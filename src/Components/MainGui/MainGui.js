de5f0a30f9937ddeb15bb3797abf7a899940e4a2
merdernoty <133769530+merdernoty@users.noreply.github.com>
2023-09-02 20:40:26 +0300

    commit Fix Main Menu

--- src/Components/MainGui/MainGui.js
+++ src/Components/MainGui/MainGui.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import styles from './MainGui.module.scss'
-import logoImage from "../../img/LogoMain.png";
+import React, { useState, useEffect } from 'react';
+import styles from './MainGui.module.scss';
+import logoImage from '../../img/LogoMain.png';
 import classNames from 'classnames';
 
 export const MainGui = () => {
-    return (
-        <header className={styles.MainWrapper}>
-            <div className={styles.MainLogo}>
-                <a href='/'>GregasShop</a>
-            </div>
-            <ul className={styles.MenuWrapper}>
-               <li>Коллекции</li>
-               <li>Хиты</li>
-               <li>Sale</li>
-               <li>Новые поступления</li>
-            </ul>
-        </header>
-    );
-}
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const headerClasses = classNames(styles.MainWrapper, {
+    [styles.sticky]: isSticky,
+  });
+
+  return (
+    <header className={headerClasses}>
+      <div className={styles.MainLogo}>GregasShop</div>
+      <ul className={styles.MenuWrapper}>
+        <li>Коллекции</li>
+        <li>Хиты</li>
+        <li>Sale</li>
+        <li>Новые поступления</li>
+      </ul>
+    </header>
+  );
+};
