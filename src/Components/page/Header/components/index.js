9554c21f5cc751cf5b0ac8fce8eec772bf3a4625
merdernoty <133769530+merdernoty@users.noreply.github.com>
2023-09-14 21:42:08 +0300

    commit Menuright fix

--- src/Components/page/Header/components/index.js
+++ src/Components/page/Header/components/index.js
@@ -3,5 +3,8 @@ import { Catalog } from './Catalog';
 import { Locate } from './Locate';
 import { MainGui } from './MainGui';
 import { Search } from './Search';
+import { Cart } from './Menuright';
+import { Favorite} from './Menuright';
+import { UserProfile } from './Menuright';
 
-export { BackToTopButton, Catalog, Locate, Search, MainGui };
+export { BackToTopButton, Catalog, Locate, Search, MainGui, Cart, Favorite, UserProfile};
