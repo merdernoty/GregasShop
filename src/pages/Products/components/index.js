38104e7ce540deba86a21f7965211bd2c7d071d5
Sedoy951 <plotkin.92@inbox.ru>
2023-09-16 18:59:50 +0300

    commit NewsWall

--- src/pages/Products/components/index.js
+++ src/pages/Products/components/index.js
@@ -1,7 +1,8 @@
 import { FotoButton } from './FotoButton';
 import { Hit } from './Hit';
 import { New } from './New';
+import { NewsWall } from './NewsWall';
 import { Slider } from './Slider';
 import { Text } from './Text';
 
-export { FotoButton, Text, Slider, New, Hit };
+export { FotoButton, Text, Slider, New, NewsWall, Hit };
