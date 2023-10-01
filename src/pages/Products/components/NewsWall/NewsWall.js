9e47559d031bb7151ddb8208b92e9bf5003c0cdf
Sedoy951 <plotkin.92@inbox.ru>
2023-10-01 18:36:24 +0300

    new Footer

--- src/pages/Products/components/NewsWall/NewsWall.js
+++ src/pages/Products/components/NewsWall/NewsWall.js
@@ -5,38 +5,38 @@ import News from '../../../../assets/images/NewsWall.jpg';
 export const NewsWall = () => {
   return (
     <div className="HitNewsWall">
-    <div className="NewsWallText">Новости</div>
-    <div className="NewsWallPhotos">
-      <div className="data-container">
-        <a href="/home">
-          <div className='image-containerNewsWallOne'>
-            <img src={News} alt="News" />
-            <div className='Date'>31.01.2002</div>
-            <p>Note that the development build is not optimized.</p>
-          </div>
-        </a>
-      </div>
-      <div className="data-container">
-        <a href="/home">
-          <div className='image-containerNewsWallTwo'>
-            <img src={News} alt="News" />
-            <div className='Date'>31.01.2002</div>
-            <p>Note that the development build is not optimized.</p>
-          </div>
-        </a>
-      </div>
-      <div className="data-container">
-        <a href="/home">
-          <div className='image-containerNewsWallThree'>
-            <img src={News} alt="News" />
-            <div className='Date'>31.01.2002</div>
-            <p>Note that the development build is not optimized.</p>
-          </div>
-        </a>
+      <div className="NewsWallText">Новости</div>
+      <div className="NewsWallPhotos">
+        <div className="data-container">
+          <a href="/home">
+            <div className="image-containerNewsWallOne">
+              <img src={News} alt="News" />
+              <div className="Date">31.01.2002</div>
+              <p>Note that the development build is not optimized.</p>
+            </div>
+          </a>
+        </div>
+        <div className="data-container">
+          <a href="/home">
+            <div className="image-containerNewsWallTwo">
+              <img src={News} alt="News" />
+              <div className="Date">31.01.2002</div>
+              <p>Note that the development build is not optimized.</p>
+            </div>
+          </a>
+        </div>
+        <div className="data-container">
+          <a href="/home">
+            <div className="image-containerNewsWallThree">
+              <img src={News} alt="News" />
+              <div className="Date">31.01.2002</div>
+              <p>Note that the development build is not optimized.</p>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
-  </div>
   );
-}
+};
 
 export default NewsWall;
