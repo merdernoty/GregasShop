85bd1633f305c045db13f6ed4201ea57ca37f65d
merdernoty <133769530+merdernoty@users.noreply.github.com>
2023-11-26 23:59:20 +0300

    commit typescript and fix main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.scss';
-import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import { routes } from './Routes';
 
--- src/layouts/BaseLayout/BaseLayout.tsx
+++ src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Header } from '../../Components';
 import { Outlet } from 'react-router-dom';
 import { Footer } from '../../Components';
