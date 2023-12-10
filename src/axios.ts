5fb189e04db7a5dc6e4e246dab9d42cbfc9ae0a9
Roma8642 <141335182+Roma8642@users.noreply.github.com>
2023-12-10 17:44:18 +0200

    .

--- src/axios.ts
+++ src/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'https://tan-bison-tam.cyclic.app/'
-    baseURL: 'http://localhost:4444'
+    baseURL: 'https://tan-bison-tam.cyclic.app/'
+    // baseURL: 'http://localhost:4444'
 
 
 })
