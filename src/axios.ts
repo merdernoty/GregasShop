ac2ff96c8967fd42320e2b7d79bc6126e5481a60
Kycedron <138807475+Kycedron@users.noreply.github.com>
2023-12-10 15:08:05 +0300

    Product

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://tan-bison-tam.cyclic.app/'
+    baseURL: 'http://localhost:4444/'
 })
 
 export default instance;
