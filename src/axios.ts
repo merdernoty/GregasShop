65aabc8e9c065fc50e53bb74250bb713df30e942
merdernoty <133769530+merdernoty@users.noreply.github.com>
2023-12-12 00:02:06 +0300

    fix api

--- src/axios.ts
+++ src/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'https://backend-bay-three.vercel.app/'
-    baseURL: 'http://localhost:4444/'
+    baseURL: 'https://backend-bay-three.vercel.app/'
+    // baseURL: 'http://localhost:4444/'
 })
 
 export default  instance;
